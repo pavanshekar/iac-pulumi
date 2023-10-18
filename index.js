0faf835d9c7252b987ba44de2136046ac332d4a8
Pavan-Soma-Shekar <shekar.p@northeastern.edu>
2023-10-18 13:00:41 -0400

    Added the pulumi code to create Application Security Group and EC2 Instance

--- index.js
+++ index.js
@@ -2,40 +2,37 @@ const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
 
 const config = new pulumi.Config();
-const availableZonesPromise = aws.getAvailabilityZones();
+const vpcCidrBlock = config.require("vpcCidrBlock");
+const destinationCidrBlock = config.require("destinationCidrBlock");
+const amiId = config.require("amiId");
+
+const currentRegion = aws.getRegion(); 
+
+const availableZonesPromise = aws.getAvailabilityZones({ region: currentRegion });
 
 availableZonesPromise.then(availableZones => {
-    const numberOfSubnets = config.getNumber("numberOfSubnets");
     const numberOfAZs = availableZones.names.length;
-    const vpcCidrBlock = config.require("vpcCidrBlock");
-    
-    const calculateSubnets = (vpcCidr, numberOfSubnets) => {
-        const [baseIp, subnetSize] = vpcCidr.split('/');
-        const subnetBits = Math.ceil(Math.log2(numberOfSubnets));
-        const newSubnetSize = parseInt(subnetSize) + subnetBits;
-        
-        if (newSubnetSize > 30) throw new Error("Subnet size is too small for the number of subnets.");
-        
-        const ipParts = baseIp.split('.').map(Number);
-        const subnets = Array.from({ length: numberOfSubnets }, (_, i) => {
-            const subnetIp = [
-                ipParts[0],
-                ipParts[1],
-                i << (8 - subnetBits),
-                0
-            ].join('.');
-            return `${subnetIp}/${newSubnetSize}`;
-        });
 
-        return subnets;
-    };
-    
     const vpc = new aws.ec2.Vpc("myVpc", {
         cidrBlock: vpcCidrBlock,
         enableDnsSupport: true,
         enableDnsHostnames: true,
         tags: {
             Name: "myVpc",
+        },
+    });
+
+    const appSecurityGroup = new aws.ec2.SecurityGroup("appSecurityGroup", {
+        vpcId: vpc.id,
+        description: "Security group for application servers",
+        ingress: [
+            { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+            { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+            { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] },
+            { protocol: "tcp", fromPort: 8080, toPort: 8080, cidrBlocks: ["0.0.0.0/0"] },
+        ],
+        tags: {
+            Name: "applicationSecurityGroup",
         },
     });
 
@@ -55,7 +52,7 @@ availableZonesPromise.then(availableZones => {
 
     const publicRoute = new aws.ec2.Route("publicRoute", {
         routeTableId: publicRouteTable.id,
-        destinationCidrBlock: "0.0.0.0/0",
+        destinationCidrBlock: destinationCidrBlock,
         gatewayId: igw.id,
     });
 
@@ -66,8 +63,6 @@ availableZonesPromise.then(availableZones => {
         },
     });
 
-    const subnets = calculateSubnets(vpcCidrBlock, numberOfSubnets);
-    
     const resources = {
         vpcId: vpc.id,
         internetGatewayId: igw.id,
@@ -75,28 +70,57 @@ availableZonesPromise.then(availableZones => {
         routeTables: [publicRouteTable.id, privateRouteTable.id],
     };
 
-    for (let i = 0; i < numberOfSubnets; i++) {
-        const az = availableZones.names[i % numberOfAZs];
-        const subnetType = i % 2 === 0 ? "public" : "private";
-        const subnetCidrBlock = subnets[i];
+    let thirdOctet = 1;
 
-        const subnet = new aws.ec2.Subnet(`${subnetType}Subnet${i}`, {
-            vpcId: vpc.id,
-            cidrBlock: subnetCidrBlock,
-            availabilityZone: az,
-            mapPublicIpOnLaunch: subnetType === "public",
-            tags: {
-                Name: `${subnetType}Subnet${i}`,
-            },
+    for (let i = 0; i < numberOfAZs; i++) {
+        const az = availableZones.names[i];
+
+        ['public', 'private'].forEach(type => {
+            const subnetCidrBlock = `10.0.${thirdOctet}.0/24`;
+
+            const subnet = new aws.ec2.Subnet(`${type}Subnet${thirdOctet}`, {
+                vpcId: vpc.id,
+                cidrBlock: subnetCidrBlock,
+                availabilityZone: az,
+                mapPublicIpOnLaunch: type === "public",
+                tags: {
+                    Name: `${type}Subnet${thirdOctet}`,
+                },
+            });
+
+            new aws.ec2.RouteTableAssociation(`${type}RouteTableAssociation${thirdOctet}`, {
+                subnetId: subnet.id,
+                routeTableId: type === "public" ? publicRouteTable.id : privateRouteTable.id,
+            });
+
+            resources.subnets.push(subnet.id);
+            thirdOctet += 2; 
         });
-
-        new aws.ec2.RouteTableAssociation(`${subnetType}RouteTableAssociation${i}`, {
-            subnetId: subnet.id,
-            routeTableId: subnetType === "public" ? publicRouteTable.id : privateRouteTable.id,
-        });
-
-        resources.subnets.push(subnet.id);
     }
+
+    const ec2Instance = new aws.ec2.Instance("appInstance", {
+        ami: amiId, 
+        instanceType: "t2.micro",
+        keyName: "key-pair",
+        vpcSecurityGroupIds: [appSecurityGroup.id],
+        subnetId: resources.subnets[0],
+        rootBlockDevice: {
+            volumeSize: 25,
+            volumeType: "gp2",
+        },
+        disableApiTermination: false, 
+        tags: {
+            Name: "ApplicationInstance",
+        },
+    });
+    
+
+    exports.ec2InstanceDetails = {
+        id: ec2Instance.id,
+        publicIp: ec2Instance.publicIp,
+        privateIp: ec2Instance.privateIp,
+        instanceType: ec2Instance.instanceType
+    };
 
     exports.resources = resources;
 });
