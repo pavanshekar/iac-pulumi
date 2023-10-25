813b423a0ccdea00f79dfd470c640a4031432aaf
Pavan-Soma-Shekar <shekar.p@northeastern.edu>
2023-10-25 12:32:02 -0400

    Added RDS

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
+const mysql = require("@pulumi/aws/rds");
 
 const config = new pulumi.Config();
 const vpcCidrBlock = config.require("vpcCidrBlock");
 const destinationCidrBlock = config.require("destinationCidrBlock");
 const amiId = config.require("amiId");
 
-const currentRegion = aws.getRegion(); 
+const currentRegion = aws.getRegion();
+
+const engine = config.require("engine");
+const instanceClass = config.require("instanceClass");
+const dbName = config.require("dbName");
+const identifier = config.require("identifier");
+const username = config.require("username");
+const dbPassword = config.requireSecret("dbPassword");
+
+const instanceType = config.require("instanceType");
+const keyName = config.require("keyName");
+const volumeSize = config.require("volumeSize");
+const volumeType = config.require("volumeType");
 
 const availableZonesPromise = aws.getAvailabilityZones({ region: currentRegion });
 
@@ -30,6 +43,9 @@ availableZonesPromise.then(availableZones => {
             { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
             { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] },
             { protocol: "tcp", fromPort: 8080, toPort: 8080, cidrBlocks: ["0.0.0.0/0"] },
+        ],
+        egress: [
+            { protocol: "tcp", fromPort: 3306, toPort: 3306, cidrBlocks: ["0.0.0.0/0"] },
         ],
         tags: {
             Name: "applicationSecurityGroup",
@@ -66,7 +82,8 @@ availableZonesPromise.then(availableZones => {
     const resources = {
         vpcId: vpc.id,
         internetGatewayId: igw.id,
-        subnets: [],
+        publicSubnets: [],
+        privateSubnets: [],
         routeTables: [publicRouteTable.id, privateRouteTable.id],
     };
 
@@ -93,36 +110,120 @@ availableZonesPromise.then(availableZones => {
                 routeTableId: type === "public" ? publicRouteTable.id : privateRouteTable.id,
             });
 
-            resources.subnets.push(subnet.id);
-            thirdOctet += 2; 
+            if (type === "public") {
+                resources.publicSubnets.push(subnet.id);
+            } else {
+                resources.privateSubnets.push(subnet.id);
+            }
+
+            thirdOctet += 2;
         });
     }
 
-    const ec2Instance = new aws.ec2.Instance("appInstance", {
-        ami: amiId, 
-        instanceType: "t2.micro",
-        keyName: "key-pair",
-        vpcSecurityGroupIds: [appSecurityGroup.id],
-        subnetId: resources.subnets[0],
-        rootBlockDevice: {
-            volumeSize: 25,
-            volumeType: "gp2",
+    const dbSecurityGroup = new aws.ec2.SecurityGroup("dbSecurityGroup", {
+        vpcId: vpc.id,
+        ingress: [
+            { protocol: "tcp", fromPort: 3306, toPort: 3306, securityGroups: [appSecurityGroup.id] },
+        ],
+        tags: {
+            Name: "databaseSecurityGroup",
         },
-        disableApiTermination: false, 
+    });
+
+    const dbParameterGroup = new mysql.ParameterGroup("db-parameter-group", {
+        family: "mariadb10.6", 
+        parameters: [{ name: "character_set_client", value: "utf8" }],
+    });
+
+    const dbSubnetGroup = new aws.rds.SubnetGroup("db-subnet-group", {
+        subnetIds: resources.privateSubnets,
+    });
+
+    const rdsInstance = new aws.rds.Instance("myRdsInstance", {
+        engine: engine,
+        instanceClass: instanceClass,
+        dbName: dbName,
+        identifier: identifier,
+        username: username,
+        password: dbPassword,
+        dbSubnetGroupName: dbSubnetGroup.name,
+        vpcSecurityGroupIds: [dbSecurityGroup.id],
+        skipFinalSnapshot: true,
+        parameterGroupName: dbParameterGroup.name,
+        allocatedStorage: 5,
+        multiAz: false,
+        publiclyAccessible: false,
+    });
+
+    const userData = pulumi.all([rdsInstance.endpoint, rdsInstance.port, rdsInstance.dbName, rdsInstance.username, rdsInstance.password]).apply(([endpoint, port, dbName, username, password]) => {
+        const [hostname] = endpoint.split(":");
+        return `#!/bin/bash
+    echo "DB_HOST=${hostname}" >> /opt/AssignmentsAPI/AssignmentsAPI/.env
+    echo "DB_PORT=${port}" >> /opt/AssignmentsAPI/AssignmentsAPI/.env
+    echo "DB_USER=${username}" >> /opt/AssignmentsAPI/AssignmentsAPI/.env
+    echo "DB_PASSWORD=${password}" >> /opt/AssignmentsAPI/AssignmentsAPI/.env
+    echo "DB_DATABASE=${dbName}" >> /opt/AssignmentsAPI/AssignmentsAPI/.env
+    sudo systemctl daemon-reload
+    sudo systemctl enable assignments-api
+    sudo systemctl start assignments-api
+    `;
+    });
+    const ec2Instance = new aws.ec2.Instance("appInstance", {
+        ami: amiId,
+        instanceType: instanceType,
+        keyName: keyName,
+        vpcSecurityGroupIds: [appSecurityGroup.id],
+        subnetId: resources.publicSubnets[0],
+        userData: userData,
+        rootBlockDevice: {
+            volumeSize: volumeSize,
+            volumeType: volumeType,
+        },
+        disableApiTermination: false,
         tags: {
             Name: "ApplicationInstance",
         },
     });
-    
+
+    exports.vpcDetails = {
+        id: vpc.id,
+        cidrBlock: vpc.cidrBlock,
+    };
+
+    exports.publicSubnetDetails = resources.publicSubnets.map((id, index) => ({
+        [`publicSubnet${index + 1}`]: id,
+    }));
+
+    exports.privateSubnetDetails = resources.privateSubnets.map((id, index) => ({
+        [`privateSubnet${index + 1}`]: id,
+    }));
+
+    exports.routeTableDetails = {
+        publicRouteTableId: publicRouteTable.id,
+        privateRouteTableId: privateRouteTable.id,
+    };
+
+    exports.securityGroupDetails = {
+        appSecurityGroupId: appSecurityGroup.id,
+        dbSecurityGroupId: dbSecurityGroup.id,
+    };
+
+    exports.dbSubnetGroupDetails = {
+        dbSubnetGroupName: dbSubnetGroup.name,
+        subnetIds: dbSubnetGroup.subnetIds,
+    };
+
+    exports.dbInstanceDetails = {
+        id: rdsInstance.id,
+        endpoint: rdsInstance.endpoint,
+        port: rdsInstance.port,
+        username: rdsInstance.username,
+        password: rdsInstance.password,
+    };
 
     exports.ec2InstanceDetails = {
         id: ec2Instance.id,
         publicIp: ec2Instance.publicIp,
         privateIp: ec2Instance.privateIp,
-        instanceType: ec2Instance.instanceType
     };
-
-    exports.resources = resources;
 });
-
-exports.availableZones = availableZonesPromise.then(availableZones => availableZones.names);
